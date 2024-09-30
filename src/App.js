22e96e6982d8c35c9c0d98b3396d1a2eb42cdf24
nehldotjs <nelsonosuya11@gmail.com>
2024-09-30 11:06:52 +0100

    NEW COMMIT

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import UserAuthenticationScreen from "./Components/Login/UserAuthenticationScree
 import userEvent from "@testing-library/user-event";
 
 function App() {
-  const [userAuth, setUserAuth] = useState(false);
+  const [userAuth, setUserAuth] = useState(true);
   return (
     <div className="App">
       {!userAuth ? (
